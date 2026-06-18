09710858db002414261629edbc510b53e94b4f44
miguelmeller <miguelmeller670@gmail.com>
2026-06-18 10:02:45 -0300

    Adicionei um codigo para pedir o nome, quantidade e localização

--- estoque.py
+++ estoque.py
@@ -1,28 +1,40 @@
 ##Definir as varíaveis
-## id, nome quantidade, localização
+## id, nome, quantidade, localização 
 produto = [
-    [1, "Martelo", 5, "Prateleira 01"],
-    [2, "Alicate", 4, "Prateleira 02"],
-    [3, "Martelo", 3, "Prateleira 03"]
+    [1, "Pão de forma", 5, "Prateleira 01"],
+    [2, "Bolacha", 4, "Prateleira 02"],
+    [3, "Macarrão", 3, "Prateleira 03"]
 
 ]
 
+
+
+proximoId = 1
 ## Definir funções:
 
 def registrarProdutos():
+    global proximoId
     ##Essa funções pergunta o nome do produto e adicona na lista de produtos
     novoProduto = input("Digite o nome do produto: ") ##Pergunta qual produto
-    produto.append(novoProduto) ##Inserimos o produto
+    ###produto.append(novoProduto) ##Inserimos o produto
+    quantidade = input("Digite  a quantidade: ")
+    localizacao = input("Digite a localização: ")
+    
+    produto.append([novoProduto, quantidade, localizacao])
+    proximoId = proximoId + 1
     print("Produto inserido com sucesso!")
 
 
 def listarProdutos():
     ##Lista os produtos registrados
     print("\n----- PRODUTOS LISTADOS -----")
-    print(f"Produtos disponíveis: {produto}")
+    #print(f"Produtos disponíveis: {produto}")
+    for item in produto: ##Para cada item, ele mostra no formato abaixo e soma no total
+        print(f"ID: {item[0]} | {item[1]} | {item[2]} ")
     print("--------------------------------")
 
-def 
+
+    
 ##Criar um menu
 
 while True: ##Esse loop roda para sempre!
@@ -33,6 +45,8 @@ while True: ##Esse loop roda para sempre!
         registrarProdutos()
     elif (opcao == "2"):
         listarProdutos()
+    #elif (opcao == "3"):
+
 
 
 
